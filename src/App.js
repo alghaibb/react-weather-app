b416792c352e02ba7d12c5adb439572fc6fcb473
Moudi <mjaderi97@gmail.com>
2023-10-28 19:06:32 +1100

    Added top buttons for project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import UilReact from '@iconscout/react-unicons/icons/uil-react'
+import TopButtons from './components/TopButtons';
 
 function App() {
   return (
-    <div>
-      
+    <div className='mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-[#4e54c8] to-[#2c3e50] h-fit shadow-xl shadow-gray-400'>
+      <TopButtons />
     </div>
   );
 }
