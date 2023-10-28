871c9133c56b31fb8284589f694e8e7f1dd1d158
Moudi <mjaderi97@gmail.com>
2023-10-28 21:29:04 +1100

    Add Inputs component for location search and temp unit toggle

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import UilReact from '@iconscout/react-unicons/icons/uil-react'
 import TopButtons from './components/TopButtons';
+import Inputs from './components/Inputs';
 
 function App() {
   return (
-    <div className='mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-[#4e54c8] to-[#2c3e50] h-fit shadow-xl shadow-gray-400'>
+    <div className='mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-[#1488CC] to-[#2B32B2] h-fit shadow-xl shadow-gray-400 selection:bg-blue-500 selection:dark:bg-gray-300'>
       <TopButtons />
+      <Inputs />
     </div>
   );
 }
